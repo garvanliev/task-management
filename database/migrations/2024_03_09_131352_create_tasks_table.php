2d461b1ebb4e3410f137409f11c8917f3c346c46
Slave <s.garvanliev@gmail.com>
2024-03-09 15:39:44 +0100

    Auth Breeze Package with Project Migration Files and Models

--- database/migrations/2024_03_09_131352_create_tasks_table.php
+++ database/migrations/2024_03_09_131352_create_tasks_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role')->default('user');
-            $table->rememberToken();
+            $table->foreignIdFor(\App\Models\User::class)->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->dateTime('duedate');
+            $table->foreignIdFor(App\Models\TaskStatus::class)->constrained()->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
